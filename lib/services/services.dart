ee054af54b58a153ce540e212c1b7330f3bdfec2
KevinM97 <khmina@espe.edu.ec>
2022-02-16 16:09:34 -0500

    Cambio de nombre de la app en AndroidManifest

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,4 +1,4 @@
 export 'package:mandaditos_express/services/auth_services.dart';
 export 'package:mandaditos_express/services/push_notifications_service.dart';
-
+export 'package:mandaditos_express/services/notifications_service.dart';
 
