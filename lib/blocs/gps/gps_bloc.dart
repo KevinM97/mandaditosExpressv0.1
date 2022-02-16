9b13b29a7a51c78874511489f1a960466f2456c7
jose1n8a98 <jose1n8n98@hotmail.com>
2022-02-16 13:44:08 -0500

    Notificaciones FMC

--- lib/blocs/gps/gps_bloc.dart
+++ lib/blocs/gps/gps_bloc.dart
@@ -9,77 +9,68 @@ part 'gps_event.dart';
 part 'gps_state.dart';
 
 class GpsBloc extends Bloc<GpsEvent, GpsState> {
-
   StreamSubscription? gpsServiceSubscription;
 
-  GpsBloc() : super( const GpsState(isGpsEnabled: false, isGpsPermissionGranted: false) ) {
-
-    on<GpsAndPermissionEvent>((event, emit)=> emit( state.copyWith(
-        isGpsEnabled:  event.isGpsEnabled,
-        isGpsPermissionGranted: event.isGpsPermissionGranted
-      ))
-    );
+  GpsBloc()
+      : super(const GpsState(
+            isGpsEnabled: false, isGpsPermissionGranted: false)) {
+    on<GpsAndPermissionEvent>((event, emit) => emit(state.copyWith(
+        isGpsEnabled: event.isGpsEnabled,
+        isGpsPermissionGranted: event.isGpsPermissionGranted)));
     _init();
   }
 
-  Future<void> _init() async{
+  Future<void> _init() async {
+    final gpsInitStatus = await Future.wait([
+      _checkGpsStatus(),
+      _isPermissionGranted(),
+    ]);
 
-
-      final gpsInitStatus = await Future.wait([
-        _checkGpsStatus(),
-        _isPermissionGranted(),
-      ]);
-
-
-      add(GpsAndPermissionEvent(
-        isGpsEnabled: gpsInitStatus[0], 
-        isGpsPermissionGranted: gpsInitStatus[1],
-        ));
+    add(GpsAndPermissionEvent(
+      isGpsEnabled: gpsInitStatus[0],
+      isGpsPermissionGranted: gpsInitStatus[1],
+    ));
   }
 
-  Future<bool> _isPermissionGranted() async{
+  Future<bool> _isPermissionGranted() async {
     final isGranted = await Permission.location.isGranted;
     return isGranted;
   }
 
   Future<bool> _checkGpsStatus() async {
-
     final insEnable = await Geolocator.isLocationServiceEnabled();
 
-    gpsServiceSubscription = Geolocator.getServiceStatusStream().listen((event) {     
-      final isEnabled = ( event.index == 1) ? true :false;
+    gpsServiceSubscription =
+        Geolocator.getServiceStatusStream().listen((event) {
+      final isEnabled = (event.index == 1) ? true : false;
       add(GpsAndPermissionEvent(
-        isGpsEnabled: isEnabled, 
+        isGpsEnabled: isEnabled,
         isGpsPermissionGranted: state.isGpsPermissionGranted,
-        ));
+      ));
     });
     return insEnable;
   }
 
+  Future<void> askGpsAccess() async {
+    final status = await Permission.location.request();
 
+    switch (status) {
+      case PermissionStatus.granted:
+        add(GpsAndPermissionEvent(
+            isGpsEnabled: state.isGpsEnabled, isGpsPermissionGranted: true));
+        break;
 
-
-Future<void> askGpsAccess() async{
-
-  final status = await Permission.location.request();
-
-  switch(status){
-    case PermissionStatus.granted:
- add( GpsAndPermissionEvent(isGpsEnabled: state.isGpsEnabled, isGpsPermissionGranted: true) );
-      break;
-    
-    case PermissionStatus.denied:
-    case PermissionStatus.restricted:
-    case PermissionStatus.limited:
-    case PermissionStatus.permanentlyDenied:
-      add(GpsAndPermissionEvent(isGpsEnabled: state.isGpsEnabled, isGpsPermissionGranted: false));
-      openAppSettings();
+      case PermissionStatus.denied:
+      case PermissionStatus.restricted:
+      case PermissionStatus.limited:
+      case PermissionStatus.permanentlyDenied:
+        add(GpsAndPermissionEvent(
+            isGpsEnabled: state.isGpsEnabled, isGpsPermissionGranted: false));
+        openAppSettings();
+    }
   }
 
-}
-
-
-@override
+  @override
   Future<void> close() {
     gpsServiceSubscription?.cancel();
     return super.close();
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:mandaditos_express/blocs/gps/gps_bloc.dart';
 import 'package:mandaditos_express/services/auth_services.dart';
 import 'package:mandaditos_express/services/push_notifications_service.dart';
@@ -11,10 +14,46 @@ import 'package:mandaditos_express/constants.dart';
 import 'Screens/login/check_outh_screen.dart';
 import 'services/notifications_service.dart';
 
+import 'dart:developer' as developer;
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  developer.log('Handling a background message ${message.messageId}');
+}
+
+/// Create a [AndroidNotificationChannel] for heads up notifications
+late AndroidNotificationChannel channel;
+
+/// Initialize the [FlutterLocalNotificationsPlugin] package.
+late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await PushNotificationService.initializeApp();
- /*  runApp(
+  // Set the background messaging handler early on, as a named top-level function
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  if (!kIsWeb) {
+    channel = const AndroidNotificationChannel(
+      'high_importance_channel', // id
+      'High Importance Notifications', // title
+      description:
+          'This channel is used for important notifications.', // description
+      importance: Importance.high,
+    );
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+  }
+  /*  runApp(
      MultiBlocProvider(
        providers:[
          BlocProvider(create: (context) => GpsBloc()),
@@ -23,34 +62,61 @@ void main() async {
          child: const MyApp(),
        )
      ); */
-   runApp(const AppState());
+  runApp(const AppState());
 }
-
 
 class AppState extends StatelessWidget {
   const AppState({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-          ChangeNotifierProvider(create: ( _ ) => AuthService()),
+        ChangeNotifierProvider(create: (_) => AuthService()),
       ],
       child: const MyApp(),
     );
   }
 }
+
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
+
 class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
+    _setupToken();
+    FirebaseMessaging.instance
+        .getInitialMessage()
+        .then((RemoteMessage? value) => developer.log(value.toString()));
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      if (notification != null && android != null && !kIsWeb) {
+        flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+              channelDescription: channel.description,
+              icon: 'launch_background',
+            ),
+          ),
+        );
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      developer.log('A new onMessageOpenedApp event was published!');
+    });
 
     //Context
     PushNotificationService.messagesStream.listen((message) {
@@ -59,7 +125,7 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
- @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Mandaditos Express',
@@ -75,5 +141,10 @@ class _MyAppState extends State<MyApp> {
       },
       scaffoldMessengerKey: NotificationService.messengerKey,
     );
+  }
+
+  _setupToken() async {
+    String? token = await FirebaseMessaging.instance.getToken();
+    developer.log(token ?? "");
   }
 }
